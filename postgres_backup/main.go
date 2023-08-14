a3fbb8cea578ada0c17193f48efed09cb51d5491
mathiasmantai <mmantaibusiness@gmail.com>
2023-08-14 08:59:17 +0200

    feat: using .env for postgres config

--- postgres_backup/main.go
+++ postgres_backup/main.go
@@ -5,30 +5,38 @@ import (
 	"os"
 	"fmt"
 	"time"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	currentTime := time.Now()
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading environment file")
+		os.Exit(1)
+	}
+
 
 	postgresConfig := pg.Postgres {
-		Host: "192.168.178.6",
-		Port: 5432,
-		DB: " ",
-		Username: "postgres",
-		Password: "postgres",
-		EnvPassword: "postgres",
+		Host: os.Getenv("POSTGRES_HOST"),
+		Port: os.Getenv("POSTGRES_PORT"),
+		DB: os.Getenv("POSTGRES_DATABASE"),
+		Username: os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		EnvPassword: os.Getenv("POSTGRES_ENV_PASSWORD"),
 	}
 
 	if postgresConfig.Host == "" || postgresConfig.Port == 0 || postgresConfig.DB == "" || postgresConfig.Username == "" || postgresConfig.EnvPassword == "" {
 		fmt.Println("Valid postgres config not found")
-		os.Exit(1)
+		os.Exit(2)
 	}
 
 	dump, err := pg.NewDump(&postgresConfig)
 	
 	if err != nil {
 		fmt.Println("Error creating new db dump: " + string(err.Error()))
-		os.Exit(2)
+		os.Exit(3)
 	}
 
 	dumpFileName := currentTime.Format("2006-01-02") + "_" + currentTIme.Hour() + "_" + currentTime.Minute() + "_" + currentTime.Second()
